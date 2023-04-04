5c5b8aa7ee1c23b9af01c24a47bc39427cac8c47
almaceleste <pcaptanovska@gmail.com>
2023-04-04 16:06:45 +0300

    refactor(functions.php): change function names to start with domain prefix
    Change my functions names to be started with theme domain prefix "resource_".

--- wordpress/html/wp-content/themes/resource/functions.php
+++ wordpress/html/wp-content/themes/resource/functions.php
@@ -1,31 +1,31 @@
 <?php
 
     // actions
-    add_action( 'after_setup_theme', 'theme_support' );
-    add_action( 'wp_enqueue_scripts', 'load_styles' );
-    add_action( 'wp_footer', 'load_scripts' );
+    add_action( 'after_setup_theme', 'resource_support' );
+    add_action( 'wp_enqueue_scripts', 'resource_styles' );
+    add_action( 'wp_footer', 'resource_scripts' );
 
     // filters
-    add_filter( 'document_title_separator', 'change_title_separator' );
+    add_filter( 'document_title_separator', 'resource_title_separator' );
 
     // functions
-    function change_title_separator($separator) {
-        $separator = '|';
-        return $separator;
-    }
+    function resource_scripts() {}
 
-    function load_scripts() {}
-
-    function load_styles() {
+    function resource_styles() {
         wp_enqueue_style( 'style', get_stylesheet_uri() );
         wp_enqueue_style( 'default', get_template_directory_uri() . '/css/default.css' );
     }
 
-    function theme_support() {
+    function resource_support() {
         add_theme_support( 'custom-logo', array(
             'header-text' => array( 'site-title', 'site-description' ),
         ) );
         add_theme_support( 'title-tag' );
+    }
+
+    function resource_title_separator($separator) {
+        $separator = '|';
+        return $separator;
     }
 
 ?>
