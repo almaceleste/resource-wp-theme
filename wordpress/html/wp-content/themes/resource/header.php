2c3b419c5122a176c80b826f739c7a7a49f29b2e
almaceleste <pcaptanovska@gmail.com>
2023-04-17 13:01:23 +0300

    feat(menu,logo,bvi): add menu and localized logos, delete bvi button
    Add nav menu to header.
    Change custom logo to localized one.
    Delete bvi accessibility button due to replace the Button Visual Impaired plugin
    with the One Click Accessibility plugin.

--- wordpress/html/wp-content/themes/resource/header.php
+++ wordpress/html/wp-content/themes/resource/header.php
@@ -2,17 +2,26 @@
     <span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span>
     <div class="wp-block-cover__inner-container">
         <div class="wp-block-group header-nav-block">
+            <?php wp_nav_menu( array(
+                'theme_location'    => 'header_menu',
+                'container'         => false
+            ) ) ?>
             <!-- wp:navigation {"ref":8,"icon":"menu","align":"wide","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"center"},"style":{"spacing":{"blockGap":"var:preset|spacing|40"}}} /-->
         </div>
         <div class="wp-block-site-logo">
             <?php
-                if ( function_exists( 'the_custom_logo' ) ) {
-                    the_custom_logo();
-                }
+                // if ( function_exists( 'the_custom_logo' ) ) {
+                //     the_custom_logo();
+                // }
             ?>
+            <a href="http://localhost/" class="custom-logo-link" rel="home" aria-current="page">
+                <!-- <img width="200" height="200" src="<?php _e('http://localhost/wp-content/uploads/2023/04/res-en.png', 'resource') ?>" class="custom-logo" alt="Resource LGBTQIA+"> -->
+                <img width="200" height="200" src="<?php echo esc_url( get_template_directory_uri() ) ?><?php _e('/images/logo-en.png', 'resource') ?>" class="custom-logo" alt="Resource LGBTQIA+">
+
+            </a>
         </div>
         <nav id="bvi">
-            <?php get_template_part('bvi'); ?>
+            <!-- <?php get_template_part('bvi'); ?> -->
         </nav>
     </div>
 </div>
