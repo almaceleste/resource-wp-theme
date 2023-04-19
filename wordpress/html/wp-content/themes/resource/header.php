438def9c7f1ddfcf54ffa344fee3a99c2fb58d6b
almaceleste <pcaptanovska@gmail.com>
2023-04-19 22:36:54 +0300

    chore(cleanup): remove old unnecessary `<img>`
    Change path to image to automatic and remove the old `<img>`

--- wordpress/html/wp-content/themes/resource/header.php
+++ wordpress/html/wp-content/themes/resource/header.php
@@ -15,7 +15,6 @@
                 // }
             ?>
             <a href="http://localhost/" class="custom-logo-link" rel="home" aria-current="page">
-                <!-- <img width="200" height="200" src="<?php _e('http://localhost/wp-content/uploads/2023/04/res-en.png', 'resource') ?>" class="custom-logo" alt="Resource LGBTQIA+"> -->
                 <img width="200" height="200" src="<?php echo esc_url( get_template_directory_uri() ) ?><?php _e('/images/logo-en.png', 'resource') ?>" class="custom-logo" alt="Resource LGBTQIA+">
 
             </a>
