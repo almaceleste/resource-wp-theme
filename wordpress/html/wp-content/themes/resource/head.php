27587df59d6a391938036d081e94cdf9954b13b9
almaceleste <pcaptanovska@gmail.com>
2023-04-24 22:55:11 +0300

    feat(head): create `link` tag for default icon

--- wordpress/html/wp-content/themes/resource/head.php
+++ wordpress/html/wp-content/themes/resource/head.php
@@ -1,4 +1,5 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="<?php echo esc_url( get_template_directory_uri() ) ?>/images/logo.svg" sizes="192x192">
 <?php wp_head(); ?>
