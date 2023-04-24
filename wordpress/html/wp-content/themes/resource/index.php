bdab1e4188abe527d810fc670068851788a99f81
almaceleste <pcaptanovska@gmail.com>
2023-04-24 17:02:09 +0300

    refactor(groups): move separate group blocks to the `for` loop
    Move separate group blocks to the `for` loop.
    Rename group images to match group increment.

--- wordpress/html/wp-content/themes/resource/index.php
+++ wordpress/html/wp-content/themes/resource/index.php
@@ -12,54 +12,21 @@
         <h1 class="alignwide has-text-align-center"><?php _e('Home_heading', 'resource') ?></h1>
 
         <div class="wp-block-columns alignwide resources">
-            <div class="wp-block-column">
-                <figure class="wp-block-image aligncenter size-medium">
-                    <img src="<?php echo esc_url( get_template_directory_uri() ) ?>/images/group1.png" alt="<?php _e('group1_name', 'resource') ?>">
-                </figure>
-                <h4 class="has-text-align-center"><?php _e('group1_name', 'resource') ?></h4>
-                <p class="has-text-align-center"><?php _e('group1_description', 'resource') ?></p>
-                <div class="wp-block-buttons">
-                    <div class="wp-block-button">
-                        <a class="wp-block-button__link wp-element-button"><?php _e('Read more', 'resource') ?></a>
+            <?php for ($i=1; $i <= 4; $i++) {
+                $group = 'group' . $i; ?>
+                <div class="wp-block-column">
+                    <figure class="wp-block-image aligncenter size-medium">
+                        <img src="<?php echo esc_url( get_template_directory_uri() ) ?>/images/<?php echo $group ?>.png" alt="<?php _e($group . '_name', 'resource') ?>">
+                    </figure>
+                    <h4 class="has-text-align-center"><?php _e($group . '_name', 'resource') ?></h4>
+                    <p class="has-text-align-center"><?php _e($group . '_description', 'resource') ?></p>
+                    <div class="wp-block-buttons">
+                        <div class="wp-block-button">
+                            <a class="wp-block-button__link wp-element-button"><?php _e('Read more', 'resource') ?></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="wp-block-column">
-                <figure class="wp-block-image aligncenter size-medium">
-                    <img src="<?php echo esc_url( get_template_directory_uri() ) ?>/images/group2.png" alt="<?php _e('group2_name', 'resource') ?>">
-                </figure>
-                <h4 class="has-text-align-center"><?php _e('group2_name', 'resource') ?></h4>
-                <p class="has-text-align-center"><?php _e('group2_description', 'resource') ?></p>
-                <div class="wp-block-buttons">
-                    <div class="wp-block-button">
-                        <a class="wp-block-button__link wp-element-button"><?php _e('Read more', 'resource') ?></a>
-                    </div>
-                </div>
-            </div>
-            <div class="wp-block-column">
-                <figure class="wp-block-image aligncenter size-medium">
-                    <img src="<?php echo esc_url( get_template_directory_uri() ) ?>/images/club.png" alt="<?php _e('group3_name', 'resource') ?>">
-                </figure>
-                <h4 class="has-text-align-center"><?php _e('group3_name', 'resource') ?></h4>
-                <p class="has-text-align-center"><?php _e('group3_description', 'resource') ?></p>
-                <div class="wp-block-buttons">
-                    <div class="wp-block-button">
-                        <a class="wp-block-button__link wp-element-button"><?php _e('Read more', 'resource') ?></a>
-                    </div>
-                </div>
-            </div>
-            <div class="wp-block-column">
-                <figure class="wp-block-image aligncenter size-medium">
-                    <img src="<?php echo esc_url( get_template_directory_uri() ) ?>/images/group3.png" alt="<?php _e('group4_name', 'resource') ?>">
-                </figure>
-                <h4 class="has-text-align-center"><?php _e('group4_name', 'resource') ?></h4>
-                <p class="has-text-align-center"><?php _e('group4_description', 'resource') ?></p>
-                <div class="wp-block-buttons">
-                    <div class="wp-block-button">
-                        <a class="wp-block-button__link wp-element-button"><?php _e('Read more', 'resource') ?></a>
-                    </div>
-                </div>
-            </div>
+            <?php } ?>
         </div>
         <?php get_sidebar() ?>
     </main>
