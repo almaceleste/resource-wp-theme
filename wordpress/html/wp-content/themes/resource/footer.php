09daaae3b31eb7e53f4b269bb1f58f3aec296fad
almaceleste <pcaptanovska@gmail.com>
2023-04-26 17:40:23 +0300

    refactor(footer): change static footer heading to localized
    Change the static footer heading to localized one.
    Change the footer logo from uploaded image to .svg image from the theme.

--- wordpress/html/wp-content/themes/resource/footer.php
+++ wordpress/html/wp-content/themes/resource/footer.php
@@ -1,12 +1,14 @@
 <div class="wp-block-group">
     <div class="wp-block-site-logo">
-        <?php
+        <!-- <?php
             if ( function_exists( 'the_custom_logo' ) ) {
                 the_custom_logo();
             }
-        ?>
+        ?> -->
+        <a href="http://localhost/" class="custom-logo-link" rel="home" aria-current="page">
+            <img width="200" height="200" src="<?php echo esc_url( get_template_directory_uri() ) ?>/images/logo.svg" class="custom-logo" alt="Resource LGBTQIA+" decoding="async" srcset="<?php echo esc_url( get_template_directory_uri() ) ?>/images/logo.svg" sizes="(max-width: 200px) 100vw, 200px">
+        </a>
     </div>
-    <pre class="wp-block-verse has-text-align-center">Центр социально-психологических и культурных проектов
-    «Ресурс ЛГБТКИА Москва» © 2023</pre>
-    <p class="created-by has-text-align-right"> ©  <a href="https://almaceleste.github.io" data-type="URL" data-id="https://almaceleste.github.io" target="_blank" rel="noreferrer noopener">almaceleste</a>, 2023</p>
+    <pre class="wp-block-verse has-text-align-center"><?php _e('Footer_heading', 'resource') ?></pre>
+    <p class="created-by has-text-align-right"> 🄯 <a href="https://almaceleste.github.io" data-type="URL" data-id="https://almaceleste.github.io" target="_blank" rel="noreferrer noopener">almaceleste</a>, 2023</p>
 </div>
