5cda64af41335d29b1ec40df82d123e09ab1077a
almaceleste <pcaptanovska@gmail.com>
2023-05-03 15:42:54 +0300

    chore(l10n): add `dummy-strings.php` to collect dummy-strings
    Create `dummy-strings.php` and add to it localization strings
    that generated by the code so `Loco Translate` doesn't see them
    and tries to delete.

---
+++ wordpress/html/wp-content/themes/resource/dummy-stings.php
@@ -0,0 +1,10 @@
+<?php
+    __('group1_description', 'resource');
+    __('group1_name', 'resource');
+    __('group2_description', 'resource');
+    __('group2_name', 'resource');
+    __('group3_description', 'resource');
+    __('group3_name', 'resource');
+    __('group4_description', 'resource');
+    __('group4_name', 'resource');
+?>
